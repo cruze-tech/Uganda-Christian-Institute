ba17867fffb1e282e57373b5c998c548b47bf86c
Gabriel <gabparsley@gmail.com>
2026-03-06 10:05:09 +0300

    Ready for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://uci-lira-demo.netlify.app',
   output: 'static',
+  devToolbar: {
+    enabled: false
+  },
   vite: {
     plugins: [tailwindcss()],
     server: {
